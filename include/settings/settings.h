5b9308c3fd4c7e311e501535467e3d714a3e352a
Mark-Walen <mark_walen@qq.com>
2024-12-19 18:05:32 +0800

    feat: settings.

---
+++ include/settings/settings.h
@@ -0,0 +1,159 @@
+//
+// Created by Mark-Walen on 2024/12/19.
+//
+
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <variant>
+#include <vector>
+
+#include "vision/helpers/yaml.h"
+
+namespace vlue::settings {
+    struct Config {
+    public:
+        virtual ~Config() = default;
+
+        Config() = default;
+
+        explicit Config([[maybe_unused]] const YAML::Node &config) {}
+
+        virtual std::ostream &serialize(std::ostream &os, int layer=0) const = 0;
+
+    public:
+        template<typename Tp>
+        using SharedPtr = std::shared_ptr<Tp>;
+
+    protected:
+        virtual void parseYAMLNode(const YAML::Node &config) = 0;
+    };
+
+    struct ROSConfig final : Config {
+        std::string node_name{"default"};
+        int qos{10};
+        std::string namespace_name;
+
+        struct Remapping {
+            std::string from;
+            std::string to;
+        };
+
+        std::vector<Remapping> remappings;
+
+        ROSConfig() = default;
+        // Constructor to parse YAML::Node
+        explicit ROSConfig(const YAML::Node &config) : Config(config) {
+            parseYAMLNode(config);
+        }
+        std::ostream &serialize(std::ostream &os, int layer=0) const override;
+
+    protected:
+        void parseYAMLNode(const YAML::Node &config) override;
+    };
+
+    // Camera configuration struct
+    struct CameraConfig final : Config {
+        std::string config_path;
+
+        struct Settings {
+            std::string exposure{"auto"};
+            std::string white_balance{"auto"};
+        } settings;
+
+        CameraConfig() = default;
+        explicit CameraConfig(const YAML::Node &config) : Config(config) {
+            parseYAMLNode(config);
+        }
+
+        std::ostream &serialize(std::ostream &os, int layer=0) const override;
+
+    protected:
+        void parseYAMLNode(const YAML::Node &config) override;
+    };
+
+    // Capture configuration struct
+    struct CaptureConfig final : Config {
+        std::vector<std::variant<int, std::string>> sources{};
+        bool replay{false};
+
+        struct Framerate {
+            int value{30};
+            int fallback{15};
+        } framerate;
+
+        struct Resolution {
+            int width;
+            int height;
+        } resolution{640, 480};
+
+        struct Save {
+            bool enable{false};
+            std::string save_path;
+            std::string format{"mp4"};
+            std::string codec{"H264"};
+            uint64_t max_file_size{1024 * 1024}; // In bytes
+        } save;
+
+        CaptureConfig() : sources(1, 0) {}
+        explicit CaptureConfig(const YAML::Node &config) : Config(config) {
+            parseYAMLNode(config);
+        }
+
+        std::ostream &serialize(std::ostream &os, int layer=0) const override;
+
+    protected:
+        void parseYAMLNode(const YAML::Node &config) override;
+    };
+
+    // Logging configuration struct
+    struct LoggingConfig final : Config {
+        std::string level{"info"};
+        std::string file;
+
+        LoggingConfig() = default;
+        explicit LoggingConfig(const YAML::Node &config) : Config(config) {
+            parseYAMLNode(config);
+        }
+
+        std::ostream &serialize(std::ostream &os, int layer=0) const override;
+
+    protected:
+        void parseYAMLNode(const YAML::Node &config) override;
+    };
+
+    // Main configuration struct
+    struct AppConfig final : Config {
+        AppConfig();
+        explicit AppConfig(const std::string &app_config);
+        explicit AppConfig(const YAML::Node &config) : Config(config) {
+            parseYAMLNode(config);
+        }
+        std::ostream &serialize(std::ostream &os, int layer=0) const override;
+
+        ROSConfig &getROSConfig() const { return *ros_;}
+        CameraConfig &getCameraConfig() const { return *camera_;}
+        CaptureConfig &getCaptureConfig() const { return *capture_;}
+        LoggingConfig &getLoggingConfig() const { return *logging_;}
+
+    protected:
+        void parseYAMLNode(const YAML::Node &config) override;
+
+    private:
+        SharedPtr<ROSConfig> ros_;
+        SharedPtr<CameraConfig> camera_;
+        SharedPtr<CaptureConfig> capture_;
+        SharedPtr<LoggingConfig> logging_;
+    };
+
+    // Overload << operator for Config
+    inline std::ostream &operator<<(std::ostream &os, const Config &config) {
+        return config.serialize(os);
+    }
+} // namespace vlue::settings
+
+
+#endif //SETTINGS_H
---
+++ src/settings/settings.cpp
@@ -0,0 +1,165 @@
+//
+// Created by wangxianggui on 2024/12/19.
+//
+
+#include "settings/settings.h"
+
+#include <iostream>
+#include <yaml-cpp/yaml.h>
+
+namespace vlue::settings {
+    static std::string indent(int layer) {
+        return std::string(layer * 2, ' ');
+    }
+
+    void ROSConfig::parseYAMLNode(const YAML::Node &config) {
+        if (!config) {
+            return;
+        }
+        node_name = config["node_name"].as<std::string>("default");
+        qos = config["qos"].as<int>(10);
+        namespace_name = config["namespace_name"].as<std::string>("");
+        if (config["remappings"] && config["remappings"].IsSequence()) {
+            for (const auto &remap: config["remappings"]) {
+                remappings.emplace_back(Remapping{
+                    remap["from"].as<std::string>(),
+                    remap["to"].as<std::string>()
+                });
+            }
+        }
+    }
+
+    // Override the serialize method
+    std::ostream &ROSConfig::serialize(std::ostream &os, int layer) const {
+        os << indent(layer) << "ROSConfig:\n";
+        os << indent(layer + 1) << "node_name: " << node_name << "\n";
+        os << indent(layer + 1) << "qos: " << qos << "\n";
+        os << indent(layer + 1) << "namespace_name: " << namespace_name << "\n";
+        os << indent(layer + 1) << "remappings:\n";
+        for (const auto &[from, to]: remappings) {
+            os << indent(layer + 2) << "from: " << from << ", to: " << to << "\n";
+        }
+        return os;
+    }
+
+    void CameraConfig::parseYAMLNode(const YAML::Node &config) {
+        config_path = config["config"].as<std::string>();
+
+        YAML::Node settings_ = config["settings"];
+        if (settings_)
+            return;
+        settings.exposure = settings_["exposure"].as<std::string>();
+        settings.white_balance = settings_["white_balance"].as<std::string>();
+    }
+
+    std::ostream &CameraConfig::serialize(std::ostream &os, int layer) const {
+        os << indent(layer) << "CameraConfig:\n"
+                << indent(layer + 1) << "config_path: " << config_path << "\n"
+                << indent(layer + 1) << "settings:\n"
+                << indent(layer + 1) << "exposure: " << settings.exposure << "\n"
+                << indent(layer + 1) << "white_balance: " << settings.white_balance << "\n";
+        return os;
+    }
+
+    void CaptureConfig::parseYAMLNode(const YAML::Node &config) {
+        if (!config) {
+            return;
+        }
+        YAML::Node sources_ = config["sources"];
+        YAML::Node framerate_ = config["framerate"];
+        YAML::Node resolution_ = config["resolution"];
+        YAML::Node save_ = config["save"];
+        if (sources_) {
+            for (const auto &source: sources_) {
+                try {
+                    sources.emplace_back(source.as<int>());
+                } catch (...) {
+                    sources.emplace_back(source.as<std::string>());
+                }
+            }
+        } else {
+            sources.emplace_back(0);
+        }
+        replay = config["replay"].as<bool>(false);
+        if (resolution_) {
+            resolution.width = resolution_["width"].as<int>(640);
+            resolution.height = resolution_["height"].as<int>(480);
+        }
+        if (save_) {
+            save.enable = save_["enable"].as<bool>(false);
+            save.save_path = save_["save_path"].as<std::string>();
+            save.format = save_["format"].as<std::string>("mp4");
+            save.codec = save_["codec"].as<std::string>("H264");
+            save.max_file_size = save_["max_file_size"].as<uint64_t>(1024 * 1024);
+        }
+    }
+
+    std::ostream &CaptureConfig::serialize(std::ostream &os, int layer) const {
+        os << indent(layer) << "CaptureConfig:\n";
+        os << indent(layer + 1) << "sources: [";
+        for (size_t i = 0; i < sources.size(); ++i) {
+            if (std::holds_alternative<int>(sources[i])) {
+                os << std::get<int>(sources[i]);
+            } else {
+                os << std::get<std::string>(sources[i]);
+            }
+            if (i < sources.size() - 1) os << ", ";
+        }
+        os << "]\n"
+                << indent(layer + 1) << "replay: " << (replay ? "true" : "false") << "\n"
+                << indent(layer + 1) << "framerate:\n"
+                << indent(layer + 2) << "value: " << framerate.value << "\n"
+                << indent(layer + 2) << "fallback: " << framerate.fallback << "\n"
+                << indent(layer + 1) << "resolution:\n"
+                << indent(layer + 2) << "width: " << resolution.width << "\n"
+                << indent(layer + 2) << "height: " << resolution.height << "\n"
+                << indent(layer + 1) << "save:\n"
+                << indent(layer + 2) << "enable: " << (save.enable ? "true" : "false") << "\n"
+                << indent(layer + 2) << "save_path: " << save.save_path << "\n"
+                << indent(layer + 2) << "format: " << save.format << "\n"
+                << indent(layer + 2) << "codec: " << save.codec << "\n"
+                << indent(layer + 2) << "max_file_size: " << save.max_file_size << "\n";
+        return os;
+    }
+
+    void LoggingConfig::parseYAMLNode(const YAML::Node &config) {
+        level = config["level"].as<std::string>("info");
+        file = config["file"].as<std::string>("");
+    }
+
+    std::ostream &LoggingConfig::serialize(std::ostream &os, int layer) const {
+        os << indent(layer) << "LoggingConfig:\n"
+                << indent(layer + 1) << "level: " << level << "\n"
+                << indent(layer + 1) << "file: " << file << "\n";
+        return os;
+    }
+
+    AppConfig::AppConfig() {
+        ros_ = std::make_shared<ROSConfig>();
+        camera_ = std::make_shared<CameraConfig>();
+        capture_ = std::make_shared<CaptureConfig>();
+        logging_ = std::make_shared<LoggingConfig>();
+    }
+
+    AppConfig::AppConfig(const std::string &app_config) : AppConfig(yaml::YAMLUtils::loadYamlConfig(app_config)) {
+    }
+
+    void AppConfig::parseYAMLNode(const YAML::Node &config) {
+        if (!config) {
+            return;
+        }
+        ros_ = std::make_shared<ROSConfig>(config["ros"]);
+        camera_ = std::make_shared<CameraConfig>(config["camera"]);
+        capture_ = std::make_shared<CaptureConfig>(config["capture"]);
+        logging_ = std::make_shared<LoggingConfig>(config["logging"]);
+    }
+
+    std::ostream &AppConfig::serialize(std::ostream &os, int layer) const {
+        os << indent(layer) << "AppConfig:\n";
+        ros_->serialize(os, layer + 1);
+        camera_->serialize(os, layer + 1);
+        capture_->serialize(os, layer + 1);
+        logging_->serialize(os, layer + 1);
+        return os;
+    }
+}
