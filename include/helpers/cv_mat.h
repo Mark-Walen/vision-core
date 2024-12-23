ffe66e611bfbd310d53f7c4e5b1579512329f872
Mark-Walen <mark_walen@qq.com>
2024-12-23 16:44:33 +0800

    feat: add cvt between vector and cv mat.

--- include/helpers/cv_mat.h
+++ include/helpers/cv_mat.h
@@ -6,6 +6,7 @@
 #define STEREO_VISION_ARRAY_MAT_H
 
 #include <array>
+#include <vector>
 
 namespace cv {
     class Mat;
@@ -14,12 +15,19 @@ namespace cv {
 
 namespace vlue::array {
 
-    class ArrayMatUtils {
+    class MatUtils {
         template<typename Tp, std::size_t Nm>
         static std::array<Tp, Nm> mat2Array(cv::Mat mat);
 
         template<typename Tp, std::size_t Nm>
         static cv::Mat array2Mat(std::array<Tp, Nm> arr, unsigned int rows, unsigned int cols);
+
+        template<typename Tp>
+        static std::vector<Tp> matToVector(const cv::Mat& mat);
+
+        // Convert std::vector (1D) to cv::Mat
+        template<typename Tp>
+        static cv::Mat vectorToMat(const std::vector<Tp>& vec, unsigned int rows, unsigned int cols);
     };
 
 } // array
