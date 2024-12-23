3c86b04c7f18676691111134569720718bb0ea2a
Mark-Walen <mark_walen@qq.com>
2024-12-23 17:30:05 +0800

    update: add cvt between vector and cv mat.

--- include/helpers/cv_mat.h
+++ include/helpers/cv_mat.h
@@ -2,20 +2,21 @@
 // Created by Mark-Walen on 2024/12/16.
 //
 
-#ifndef STEREO_VISION_ARRAY_MAT_H
-#define STEREO_VISION_ARRAY_MAT_H
+#ifndef STEREO_VISION_CV_MAT_H
+#define STEREO_VISION_CV_MAT_H
 
-#include <array>
 #include <vector>
+#include <array>
 
 namespace cv {
     class Mat;
 }
 
 
-namespace vlue::array {
+namespace vlue::helpers {
 
     class MatUtils {
+    public:
         template<typename Tp, std::size_t Nm>
         static std::array<Tp, Nm> mat2Array(cv::Mat mat);
 
@@ -25,7 +26,6 @@ namespace vlue::array {
         template<typename Tp>
         static std::vector<Tp> matToVector(const cv::Mat& mat);
 
-        // Convert std::vector (1D) to cv::Mat
         template<typename Tp>
         static cv::Mat vectorToMat(const std::vector<Tp>& vec, unsigned int rows, unsigned int cols);
     };
--- src/helpers/cv_mat.cpp
+++ src/helpers/cv_mat.cpp
@@ -1,49 +1,48 @@
 //
-// Created by wangxianggui on 2024/12/16.
+// Created by Mark-Walen on 2024/12/16.
 //
 #include "helpers/cv_mat.h"
 #include <opencv2/core/mat.hpp>
 
-using namespace vlue::array;
-
-template<typename Tp, std::size_t Nm>
-std::array<Tp, Nm> MatUtils::mat2Array(cv::Mat mat) {
-    if (Nm != mat.total()) {
-        throw std::invalid_argument("Array size must match the number of elements in the matrix.");
-    }
-    std::array<Tp, Nm> arr;
-    std::memcpy(arr.data(), mat.data, mat.total() * sizeof(Tp));
-    return arr;
-}
-
-template<typename Tp, std::size_t Nm>
-cv::Mat MatUtils::array2Mat(std::array<Tp, Nm> arr, unsigned int rows, unsigned int cols) {
-    if (rows * cols != Nm) {
-        throw std::invalid_argument("Matrix dimensions do not match the array size.");
+namespace vlue::helpers {
+    template<typename Tp, std::size_t Nm>
+    std::array<Tp, Nm> MatUtils::mat2Array(cv::Mat mat) {
+        if (Nm != mat.total()) {
+            throw std::invalid_argument("Array size must match the number of elements in the matrix.");
+        }
+        std::array<Tp, Nm> arr;
+        std::memcpy(arr.data(), mat.data, mat.total() * sizeof(Tp));
+        return arr;
     }
 
-    cv::Mat mat(rows, cols, cv::DataType<Tp>::type, arr.data());
-    return mat;
-}
+    template<typename Tp, std::size_t Nm>
+    cv::Mat MatUtils::array2Mat(std::array<Tp, Nm> arr, unsigned int rows, unsigned int cols) {
+        if (rows * cols != Nm) {
+            throw std::invalid_argument("Matrix dimensions do not match the array size.");
+        }
 
-template<typename Tp>
-std::vector<Tp> MatUtils::matToVector(const cv::Mat& mat) {
-    if (mat.empty()) {
-        throw std::invalid_argument("Input matrix is empty.");
-    }
-    if (mat.type() != cv::DataType<Tp>::type) {
-        throw std::invalid_argument("Matrix type does not match the specified type.");
+        cv::Mat mat(rows, cols, cv::DataType<Tp>::type, arr.data());
+        return mat;
     }
 
-    return std::vector<Tp>(mat.begin<Tp>(), mat.end<Tp>());
-}
+    template<typename Tp>
+    std::vector<Tp> MatUtils::matToVector(const cv::Mat &mat) {
+        if (mat.empty()) {
+            throw std::invalid_argument("Input matrix is empty.");
+        }
+        if (mat.type() != cv::DataType<Tp>::type) {
+            throw std::invalid_argument("Matrix type does not match the specified type.");
+        }
 
-// Convert std::vector (1D) to cv::Mat
-template<typename Tp>
-cv::Mat vectorToMat(const std::vector<Tp>& vec, unsigned int rows, unsigned int cols) {
-    if (rows * cols != vec.size()) {
-        throw std::invalid_argument("Matrix dimensions do not match the vector size.");
+        return std::vector<Tp>(mat.begin<Tp>(), mat.end<Tp>());
     }
 
-    return cv::Mat(rows, cols, cv::DataType<Tp>::type, const_cast<Tp*>(vec.data())).clone();
+    template <typename Tp>
+    cv::Mat MatUtils::vectorToMat(const std::vector<Tp> &vec, unsigned int rows, unsigned int cols)
+    {
+        if (rows * cols != vec.size()) {
+            throw std::invalid_argument("Matrix dimensions do not match the vector size.");
+        }
+        return cv::Mat(rows, cols, cv::DataType<Tp>::type, const_cast<Tp *>(vec.data()));
+    }
 }
